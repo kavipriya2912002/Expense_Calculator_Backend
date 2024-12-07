74b2682ed099d2e656255fff57ff466ea472cbbd
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-12-07 05:30:26 -0800

    code updated

--- api/index.js
+++ api/index.js
@@ -6,13 +6,11 @@ import ConnectDB from "./config/db.js";
 import userRoutes from "./Routes/userRoutes.js"
 import travelRoutes from "./Routes/travelRoutes.js"
 import expenseRoutes from "./Routes/expenseRoutes.js"
-import path from 'path';
 dotenv.config();
 ConnectDB();
 const PORT= 3000  || process.env.PORT;
 const app = express();
 app.use(express.json());
-const __dirname = path.resolve();
 // app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -22,11 +20,6 @@ app.get('/',(req,res)=>{
 app.use('/api/user',userRoutes);
 app.use('/api/travel',travelRoutes);
 app.use('/api/expense',expenseRoutes);
-app.use(express.static(path.join(__dirname,'/client/dist')));
-
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
-});
 
 app.listen(PORT,console.log(`Server listening to port ${PORT}`));
 
